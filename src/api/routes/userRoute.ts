8adf637c804f9d5708d6aa173ff49f26c3904726
Ilkka Kylmäniemi <ilkka.kylmaniemi@metropolia.fi>
2023-09-20 11:17:56 +0300

    routet

--- src/api/routes/userRoute.ts
+++ src/api/routes/userRoute.ts
@@ -1,28 +1,26 @@
 import express from 'express';
 import {
-  check,
-  checkToken,
-  userDelete,
-  userGet,
-  userListGet,
+  // check,
+  // checkToken,
+  // userDelete,
+  // userGet,
+  // userListGet,
   userPost,
-  userPut,
+  // userPut,
 } from '../controllers/userController';
-import {authenticate} from '../../middlewares';
+// import {authenticate} from '../../middlewares';
 
 const router = express.Router();
 
-router
-  .route('/')
-  .get(userListGet)
-  .post(userPost)
-  .put(authenticate, userPut)
-  .delete(authenticate, userDelete);
+router.route('/').post(userPost);
+// .get(userListGet)
+// .put(authenticate, userPut)
+// .delete(authenticate, userDelete);
 
-router.get('/token', authenticate, checkToken);
+// router.get('/token', authenticate, checkToken);
 
-router.route('/check').get(check);
+// router.route('/check').get(check);
 
-router.route('/:id').get(userGet);
+// router.route('/:id').get(userGet);
 
 export default router;
